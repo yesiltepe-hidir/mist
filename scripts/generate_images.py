7706b89d56fa8082125952e59a682b0748c512b7
Hıdır Yeşiltepe <70890453+yesiltepe-hidir@users.noreply.github.com>
2024-05-04 18:14:21 -0400

    Create generate_images.py

---
+++ scripts/generate_images.py
@@ -0,0 +1,52 @@
+import sys
+sys.path.append('../../mist')
+from diffusers import StableDiffusionPipeline
+import torch
+import numpy as np
+from PIL import Image
+from image_utils import view_images
+import os
+
+seeds = np.random.randint(0, 5000, 1)
+seeds = [421] # 3098 4088, 3103
+models_path = '/home/grads/hidir/unified-concept-editing/appendix_checkpoints/'
+with torch.no_grad():
+        for model_name in os.listdir(models_path):
+                if model_name.find('lib_02') != -1 or model_name.find('gender_guard') == -1:
+                        continue
+                print(model_name)
+                split = model_name.split('_')
+                
+                occupation = split[-1]
+                if int(split[0]) not in [15]:
+                        continue
+                prompt = [f'a headshot of a guard']
+                saved_model = models_path + model_name
+                offset = 0
+                for seed in seeds:
+                        seed = int(seed)
+                        save_path = f'appendix_checkpoints/appendix_images/'
+                        os.makedirs(save_path, exist_ok=True)
+                        device = 'cuda'
+                        model_version = 'CompVis/stable-diffusion-v1-4'
+                        model = StableDiffusionPipeline.from_pretrained(model_version, safety_checker=None).to(device)
+                        model.unet.load_state_dict(torch.load(saved_model))
+                        g = torch.Generator(device='cpu')
+                        g.manual_seed(int(seed))
+                        # g = None
+                        num_tokens = len(prompt[0].split()) + 2
+                        prompt_input = model.tokenizer(
+                                prompt,
+                                padding="max_length",
+                                max_length=model.tokenizer.model_max_length,
+                                truncation=True,
+                                return_tensors="pt")
+                        prompt_emb = model.text_encoder(prompt_input.input_ids.to(model.device))[0]
+                        images = model(prompt_embeds=prompt_emb, negative_prompt = ['black and white, low quality'], num_images_per_prompt=51, num_inference_steps=20, generator = g).images
+                        for m in range(len(images)):
+                                if m in [33]:
+                                        images[m].save(save_path + str(offset + m) +' .png')
+                                        images[m].save(save_path + str(offset + m) +' .png')
+                        torch.cuda.empty_cache()
+                        del model
+                        
